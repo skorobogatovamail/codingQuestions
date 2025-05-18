16c3760cf04616369649cd3610be72749fa59672
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-05-18 17:03:46 +0300

    add promiseAll

--- promiseAll/index.js
+++ promiseAll/index.js
@@ -1,41 +1,3 @@
-
-// function promiseAll(promises) {
-//     return new Promise((resolve, reject) => {
-//         const res = [];
-//         let completed = 0;
-
-//         promises.forEach((promise, index) => {
-//             Promise.resolve(promise)
-//                 .then((data) => {
-//                     res[index] = data
-//                     completed += 1;
-//                     if (completed === promises.length) {
-//                         resolve(res);
-//                     }
-//                 })
-//                 .catch((error) => reject(error));
-//         });
-//     });
-// }
-
-
-// const p0 = Promise.resolve(3)
-// const p1 = 42
-// const p2 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('foo')
-//     })
-// })
-
-// // Promise.all([p0, p1, p2]).then(console.log)
-// promiseAll([p0, p1, p2]).then(console.log)
-
-
-
-
-
-
-
 const p0 = Promise.resolve(3)
 const p1 = 45
 const p2 = new Promise((resolve, reject) => {
@@ -63,3 +25,31 @@ function promiseAll(promises) {
 }
 
 promiseAll([p0, p1, p2]).then(console.log)
+
+
+
+
+function all(promises) {
+    const res = [];
+    let completed = 0
+    return new Promise((resolve, reject) => {
+        if (promises.length === 0) resolve(res);
+        promises.forEach((promise, idx) => {
+            Promise.resolve(promise)
+                .then(data => {
+                    res[idx] = data
+                    completed += 1
+                    if (completed === promises.length) resolve(res)
+                })
+                .catch(error => reject(error))
+        })
+    })
+}
+
+// async function all(promises) {
+//   let result = []
+//   for(let promise of promises){
+//     result.push(await promise)
+//   }
+//   return result;
+// }
